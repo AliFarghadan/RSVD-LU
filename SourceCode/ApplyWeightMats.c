41cbcba2f993d131ff51248be29ad8609371039e
AliFarghadan <161867326+AliFarghadan@users.noreply.github.com>
2025-01-28 20:19:33 -0500

    Update ApplyWeightMats.c

--- SourceCode/ApplyWeightMats.c
+++ SourceCode/ApplyWeightMats.c
@@ -15,35 +15,36 @@ PetscErrorCode ApplyWeightMats(RSVD_matrices *RSVD_mat, RSVD_vars *RSVD, Weight_
 
 	if (DirAdj) { // direct 
 		if (before) { // forcing 
-			if (Weight_mat->InvInputWeightFlg)  ierr = MatMatMult(Weight_mat->W_f_sqrt_inv,RSVD_mat->Y_hat,MAT_REUSE_MATRIX,PETSC_DEFAULT,&RSVD_mat->Y_hat);CHKERRQ(ierr);
+			if (Weight_mat->InvInputWeightFlg)  {
+				ierr = MatMatMult(Weight_mat->W_f_sqrt_inv,RSVD_mat->Y_hat,MAT_INITIAL_MATRIX,PETSC_DEFAULT,&Y);CHKERRQ(ierr);
+				ierr = MatCopy(Y,RSVD_mat->Y_hat,SAME_NONZERO_PATTERN);CHKERRQ(ierr);
+				ierr = MatDestroy(&Y);CHKERRQ(ierr);
+			}
 			if (Weight_mat->InputMatrixFlg)  {
 				ierr = MatMatMult(Weight_mat->B,RSVD_mat->Y_hat,MAT_INITIAL_MATRIX,PETSC_DEFAULT,&Y);CHKERRQ(ierr);
-				ierr = MatDestroy(&RSVD_mat->Y_hat);CHKERRQ(ierr);
-				ierr = MatCreate(PETSC_COMM_WORLD,&RSVD_mat->Y_hat);CHKERRQ(ierr);
-				ierr = MatSetType(RSVD_mat->Y_hat,MATDENSE);CHKERRQ(ierr);
-				ierr = MatSetSizes(RSVD_mat->Y_hat,PETSC_DECIDE,PETSC_DECIDE,RSVD->N,RSVD->k);CHKERRQ(ierr);
-				ierr = MatSetUp(RSVD_mat->Y_hat);CHKERRQ(ierr);				
-				ierr = MatCopy(Y,RSVD_mat->Y_hat,SAME_NONZERO_PATTERN);CHKERRQ(ierr);
+				ierr = MatDuplicate(Y,MAT_COPY_VALUES,&RSVD_mat->Y_hat);CHKERRQ(ierr);
 				ierr = MatDestroy(&Y);CHKERRQ(ierr);
 			}
 		} else { // response 
 			if (Weight_mat->OutputMatrixFlg) {
 				ierr = MatMatMult(Weight_mat->C,RSVD_mat->Y_hat,MAT_INITIAL_MATRIX,PETSC_DEFAULT,&Y);CHKERRQ(ierr);
 				ierr = MatDestroy(&RSVD_mat->Y_hat);CHKERRQ(ierr);
-				ierr = MatCreate(PETSC_COMM_WORLD,&RSVD_mat->Y_hat);CHKERRQ(ierr);
-				ierr = MatSetType(RSVD_mat->Y_hat,MATDENSE);CHKERRQ(ierr);
-				ierr = MatSetSizes(RSVD_mat->Y_hat,PETSC_DECIDE,PETSC_DECIDE,RSVD->Nc,RSVD->k);CHKERRQ(ierr);
-				ierr = MatSetUp(RSVD_mat->Y_hat);CHKERRQ(ierr);			
-				ierr = MatCopy(Y,RSVD_mat->Y_hat,SAME_NONZERO_PATTERN);CHKERRQ(ierr);
+				ierr = MatDuplicate(Y,MAT_COPY_VALUES,&RSVD_mat->Y_hat);CHKERRQ(ierr);
 				ierr = MatDestroy(&Y);CHKERRQ(ierr);
 			}
-			if (Weight_mat->OutputWeightFlg) ierr = MatMatMult(Weight_mat->W_q_sqrt,RSVD_mat->Y_hat,MAT_REUSE_MATRIX,PETSC_DEFAULT,&RSVD_mat->Y_hat);CHKERRQ(ierr);
+			if (Weight_mat->OutputWeightFlg) {
+				ierr = MatMatMult(Weight_mat->W_q_sqrt,RSVD_mat->Y_hat,MAT_INITIAL_MATRIX,PETSC_DEFAULT,&Y);CHKERRQ(ierr);
+				ierr = MatCopy(Y,RSVD_mat->Y_hat,SAME_NONZERO_PATTERN);CHKERRQ(ierr);
+				ierr = MatDestroy(&Y);CHKERRQ(ierr);
+			}		
 		}
 	} else { // adjoint
 		if (before) { // forcing
 			if (Weight_mat->OutputWeightFlg) {
 				ierr = MatHermitianTranspose(Weight_mat->W_q_sqrt, MAT_INPLACE_MATRIX, &Weight_mat->W_q_sqrt);CHKERRQ(ierr);
-				ierr = MatMatMult(Weight_mat->W_q_sqrt,RSVD_mat->Y_hat,MAT_REUSE_MATRIX,PETSC_DEFAULT,&RSVD_mat->Y_hat);CHKERRQ(ierr);
+				ierr = MatMatMult(Weight_mat->W_q_sqrt,RSVD_mat->Y_hat,MAT_INITIAL_MATRIX,PETSC_DEFAULT,&Y);CHKERRQ(ierr);
+				ierr = MatCopy(Y,RSVD_mat->Y_hat,SAME_NONZERO_PATTERN);CHKERRQ(ierr);
+				ierr = MatDestroy(&Y);CHKERRQ(ierr);
 				ierr = MatHermitianTranspose(Weight_mat->W_q_sqrt, MAT_INPLACE_MATRIX, &Weight_mat->W_q_sqrt);CHKERRQ(ierr);
 			}
 			if (Weight_mat->OutputMatrixFlg) {
@@ -51,11 +52,7 @@ PetscErrorCode ApplyWeightMats(RSVD_matrices *RSVD_mat, RSVD_vars *RSVD, Weight_
 				ierr = MatMatMult(Weight_mat->C,RSVD_mat->Y_hat,MAT_INITIAL_MATRIX,PETSC_DEFAULT,&Y);CHKERRQ(ierr);
 				ierr = MatHermitianTranspose(Weight_mat->C, MAT_INPLACE_MATRIX, &Weight_mat->C);CHKERRQ(ierr);
 				ierr = MatDestroy(&RSVD_mat->Y_hat);CHKERRQ(ierr);
-				ierr = MatCreate(PETSC_COMM_WORLD,&RSVD_mat->Y_hat);CHKERRQ(ierr);
-				ierr = MatSetType(RSVD_mat->Y_hat,MATDENSE);CHKERRQ(ierr);
-				ierr = MatSetSizes(RSVD_mat->Y_hat,PETSC_DECIDE,PETSC_DECIDE,RSVD->N,RSVD->k);CHKERRQ(ierr);
-				ierr = MatSetUp(RSVD_mat->Y_hat);CHKERRQ(ierr);				
-				ierr = MatCopy(Y,RSVD_mat->Y_hat,SAME_NONZERO_PATTERN);CHKERRQ(ierr);
+				ierr = MatDuplicate(Y,MAT_COPY_VALUES,&RSVD_mat->Y_hat);CHKERRQ(ierr);
 				ierr = MatDestroy(&Y);CHKERRQ(ierr);
 			}
 		} else { // response
@@ -64,16 +61,14 @@ PetscErrorCode ApplyWeightMats(RSVD_matrices *RSVD_mat, RSVD_vars *RSVD, Weight_
 				ierr = MatMatMult(Weight_mat->B,RSVD_mat->Y_hat,MAT_INITIAL_MATRIX,PETSC_DEFAULT,&Y);CHKERRQ(ierr);
 				ierr = MatHermitianTranspose(Weight_mat->B, MAT_INPLACE_MATRIX, &Weight_mat->B);CHKERRQ(ierr);
 				ierr = MatDestroy(&RSVD_mat->Y_hat);CHKERRQ(ierr);
-				ierr = MatCreate(PETSC_COMM_WORLD,&RSVD_mat->Y_hat);CHKERRQ(ierr);
-				ierr = MatSetType(RSVD_mat->Y_hat,MATDENSE);CHKERRQ(ierr);
-				ierr = MatSetSizes(RSVD_mat->Y_hat,PETSC_DECIDE,PETSC_DECIDE,RSVD->Nb,RSVD->k);CHKERRQ(ierr);
-				ierr = MatSetUp(RSVD_mat->Y_hat);CHKERRQ(ierr);				
-				ierr = MatCopy(Y,RSVD_mat->Y_hat,SAME_NONZERO_PATTERN);CHKERRQ(ierr);
+				ierr = MatDuplicate(Y,MAT_COPY_VALUES,&RSVD_mat->Y_hat);CHKERRQ(ierr);
 				ierr = MatDestroy(&Y);CHKERRQ(ierr);
 			}
 			if (Weight_mat->InvInputWeightFlg)  {
 				ierr = MatHermitianTranspose(Weight_mat->W_f_sqrt_inv, MAT_INPLACE_MATRIX, &Weight_mat->W_f_sqrt_inv);CHKERRQ(ierr);
-				ierr = MatMatMult(Weight_mat->W_f_sqrt_inv,RSVD_mat->Y_hat,MAT_REUSE_MATRIX,PETSC_DEFAULT,&RSVD_mat->Y_hat);CHKERRQ(ierr);
+				ierr = MatMatMult(Weight_mat->W_f_sqrt_inv,RSVD_mat->Y_hat,MAT_INITIAL_MATRIX,PETSC_DEFAULT,&Y);CHKERRQ(ierr);
+				ierr = MatCopy(Y,RSVD_mat->Y_hat,SAME_NONZERO_PATTERN);CHKERRQ(ierr);
+				ierr = MatDestroy(&Y);CHKERRQ(ierr);	
 				ierr = MatHermitianTranspose(Weight_mat->W_f_sqrt_inv, MAT_INPLACE_MATRIX, &Weight_mat->W_f_sqrt_inv);CHKERRQ(ierr);
 			}
 		}
@@ -82,6 +77,7 @@ PetscErrorCode ApplyWeightMats(RSVD_matrices *RSVD_mat, RSVD_vars *RSVD, Weight_
 	PetscFunctionReturn(0);
 	
 }
+
 
 
 
